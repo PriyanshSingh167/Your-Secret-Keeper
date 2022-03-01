cd4d45fa8eef07a0b53f40560e0b39332cf2eec0
priyanshsingh <priyansh@singh.com>
2022-03-01 18:52:55 +0530

    Included hash password

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -28,11 +28,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/login", (req, res) => {
@@ -46,7 +41,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   newUser.save((err) => {
     if (err) {
@@ -59,7 +54,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: userName }, (err, foundUser) => {
     if (err) {
